255977350d049f151ad7cdaa143dfee0a226cd79
Villanueva_mervic09 <79442383+Villanueva-mervic09@users.noreply.github.com>
2021-03-01 17:55:18 +0800

    Add files via upload

---
+++ Sample1_HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Sample2_MyProfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sample2_MyProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Name: Mervic Villanueva");
+
+            Console.WriteLine("Birthdate: October 10, 2000");
+
+            Console.WriteLine("Course: Bachelor of Science In Computer Science Major in Network and Data Communication");
+
+            Console.WriteLine("Year: II");
+
+            Console.WriteLine("Section: B");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Name: Jeande Sarmiento");
+
+            Console.WriteLine("Birthdate: February 09, 2001");
+
+            Console.WriteLine("Course: Bachelor of Science In Computer Science Major in Network and Data Communication");
+
+            Console.WriteLine("Year: II");
+
+            Console.WriteLine("Section: B");
+
+            Console.ReadKey();
+        }
+    }
+}
