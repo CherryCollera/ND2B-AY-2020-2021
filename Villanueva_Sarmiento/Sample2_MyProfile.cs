eb61c90b8f50574d1e30f397e50b8da697b8c902
Villanueva_mervic09 <79442383+Villanueva-mervic09@users.noreply.github.com>
2021-03-01 17:06:19 +0800

    Add files via upload

---
+++ Villanueva_Sarmiento/Sample2_MyProfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sample2_MyProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine("Enter your name: Mervic Villanueva");
+
+            Console.WriteLine("Date of Birth: October 10, 2000");
+
+            Console.WriteLine("Course: Bachelor of Science In Computer Science Major in Network and Data Communication");
+
+            Console.WriteLine("Year: II");
+
+            Console.WriteLine("Section: A");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Enter your name: Jeande Sarmiento");
+
+            Console.WriteLine("Date of Birth: February 09, 2001");
+
+            Console.WriteLine("Course: Bachelor of Science In Computer Science Major in Network and Data Communication");
+
+            Console.WriteLine("Year: II");
+
+            Console.WriteLine("Section: A");
+
+            Console.ReadKey();
+        }
+    }
+}
